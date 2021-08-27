9460a9a0ba1aad2db60a628872266f342cc4e232
Danial A <danazad2016@gmail.com>
2021-08-26 21:18:07 -0500

    Added generated tiles

--- minesweeper.js
+++ minesweeper.js
@@ -1,32 +1,37 @@
-const TILE_STATUSES = {
+const TITLE_STATUSES = {
   HIDDEN: "hidden",
   MINE: "mine",
-  MARKED: "marked",
   NUMBER: "number",
+  MARKED: "marked",
 }
 
 export function createBoard(boardSize, numberOfMines) {
   const board = []
 
   for (let x = 0; x < boardSize; x++) {
-    const row = []
+    let row = []
 
     for (let y = 0; y < boardSize; y++) {
       const element = document.createElement("div")
-      element.dataset.status = TILE_STATUSES.HIDDEN
+      element.dataset.status = TITLE_STATUSES.HIDDEN
 
       const tile = {
         element,
         x,
         y,
+        mine,
+        get status() {
+          return element.dataset.status
+        },
+        set status(value) {
+          this.element.dataset.status = value
+        },
       }
 
       row.push(tile)
     }
 
     board.push(row)
-
-    console.log(board)
   }
 
   return board
--- ui.js
+++ ui.js
@@ -1,14 +1,14 @@
 import { createBoard } from "./minesweeper.js"
 
-const BOARD_SIZE = 3
-const NUMBER_OF_MINES = 5
+const BOARD_SIZE = 5
+const NUMBER_OF_MINES = 2
 
-const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+const board = createBoard(5, 2)
 const boardElement = document.querySelector(".board")
 boardElement.style.setProperty("--size", BOARD_SIZE)
 
 board.forEach((row) => {
-  row.forEach((tile) => {
-    boardElement.append(tile.element)
+  row.forEach((title) => {
+    boardElement.append(title.element)
   })
 })
