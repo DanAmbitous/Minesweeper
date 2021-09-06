7c1fb82752fadfc7bc7db8e7431d35b2d9bbaf1b
Danial A <danazad2016@gmail.com>
2021-09-06 13:35:05 -0500

    Added mines and new range slider

--- minesweeper.js
+++ minesweeper.js
@@ -1,16 +1,37 @@
-export function boardCreation(size) {
+const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  NUMBER: "number",
+  MARKED: "marked",
+}
+
+export function boardCreation(size, mineQuantity) {
   const board = []
+
+  const minePositions = getMinePosition(size, mineQuantity)
 
   for (let x = 0; x < size; x++) {
     const row = []
 
     for (let y = 0; y < size; y++) {
       const tileElement = document.createElement("div")
+      tileElement.dataset.status = TILE_STATUSES.HIDDEN
 
       const tile = {
         tileElement,
         x,
         y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.tileElement.dataset.status
+        },
+        set status(value) {
+          this.tileElement.dataset.status = value
+        },
+      }
+
+      if (tile.mine) {
+        tile.tileElement.style.backgroundColor = "red"
       }
 
       row.push(tile)
@@ -20,4 +41,30 @@ export function boardCreation(size) {
   }
 
   return board
+}
+
+function getMinePosition(boardSize, minesQuantity) {
+  const positions = []
+
+  while (positions.length < minesQuantity) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    }
+
+    //Checking to make sure that the positions aren't a match
+    if (!positions.some((p) => positionMatch(p, position))) {
+      positions.push(position)
+    }
+  }
+
+  return positions
+}
+
+function positionMatch(positionA, positionB) {
+  return positionA.x === positionB.x && positionA.y === positionB.y
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size)
 }
--- ui.js
+++ ui.js
@@ -1,12 +1,15 @@
 import { boardCreation } from "./minesweeper.js"
 
+const mineRange = document.querySelector("#mine-range")
+
 let BOARD_SIZE = 5
+let MINE_QUANTITY = 3
 
 const boardElement = document.querySelector(".board")
 boardElement.style.setProperty("--size", BOARD_SIZE)
 
-let board = boardCreation(BOARD_SIZE)
-
+let board = boardCreation(BOARD_SIZE, MINE_QUANTITY)
+console.log(board)
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.tileElement)
@@ -22,7 +25,7 @@ document.addEventListener("input", (e) => {
 
   BOARD_SIZE = e.target.value
 
-  board = boardCreation(BOARD_SIZE)
+  board = boardCreation(BOARD_SIZE, MINE_QUANTITY)
   boardElement.style.setProperty("--size", BOARD_SIZE)
 
   board.forEach((row) => {
@@ -30,4 +33,6 @@ document.addEventListener("input", (e) => {
       boardElement.append(tile.tileElement)
     })
   })
+
+  mineRange.max = BOARD_SIZE
 })
