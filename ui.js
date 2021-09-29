501b1faa77dd61080f30c7f940184d7305e71b92
Danial A <danazad2016@gmail.com>
2021-09-29 18:46:37 -0500

    Added mine quantity

--- ui.js
+++ ui.js
@@ -4,7 +4,7 @@ let boardSizeRange = document.querySelector("#board-size-range")
 let mineQuantityRange = document.querySelector("#mine-quantity-range")
 
 let BOARD_DIMENSIONS = Number(boardSizeRange.value)
-let MINE_QUANTITY = 2
+let MINE_QUANTITY = 5
 
 let boardElement = document.querySelector(".board")
 boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
@@ -17,7 +17,7 @@ let boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
 let boardSizeIndicator = document.querySelector(".board-size")
 let mineQuantity = document.querySelector(".mine-quantity")
 
-mineQuantity.min = BOARD_DIMENSIONS
+mineQuantity.max = BOARD_DIMENSIONS
 
 boardSizeIndicator.textContent = `${BOARD_DIMENSIONS}x${BOARD_DIMENSIONS}`
 mineQuantity.textContent = MINE_QUANTITY
@@ -125,65 +125,40 @@ boardSizeRange.addEventListener("input", (e) => {
 })
 
 mineQuantityRange.addEventListener("input", (e) => {
-  // const tiles = Array.from(boardElement.children)
-  // MINE_QUANTITY = Number(boardSizeRange.value)
-  // tiles.forEach((tile) => {
-  //   tile.remove()
-  // })
-  // boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-  // boardCreation.forEach((row) => {
-  //   row.forEach((tile) => {
-  //     boardElement.append(tile.tileElement)
-  //     tile.tileElement.addEventListener("click", () => {
-  //       tileRevealing(
-  //         tile,
-  //         boardCreation,
-  //         boardElement,
-  //         statusInformer,
-  //         MINE_QUANTITY,
-  //         replayContainer
-  //       )
-  //     })
-  //     tile.tileElement.addEventListener("contextmenu", (e) => {
-  //       e.preventDefault()
-  //       tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-  //     })
-  //   })
-  // })
+  MINE_QUANTITY = Number(mineQuantityRange.value)
+
+  mineQuantity.textContent = MINE_QUANTITY
+
+  const tiles = Array.from(boardElement.children)
+
+  tiles.forEach((tile) => tile.remove())
+
+  boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
+  boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
+
+  boardCreation.forEach((row) => {
+    row.forEach((tile) => {
+      boardElement.append(tile.tileElement)
+
+      tile.tileElement.addEventListener("click", () => {
+        tileRevealing(
+          tile,
+          boardCreation,
+          boardElement,
+          statusInformer,
+          MINE_QUANTITY,
+          replayContainer
+        )
+      })
+
+      tile.tileElement.addEventListener("contextmenu", (e) => {
+        e.preventDefault()
+
+        tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
+      })
+    })
+  })
 })
-
-// import {
-//   tileCreation,
-//   tileRevealing,
-//   tileMarking,
-//   TILE_STATUSES,
-// } from "./minesweeper.js"
-
-// let BOARD_SIZE = 5
-// let MINE_QUANTITY = 1
-
-// let boardContainer = document.querySelector(".board")
-// boardContainer.style.setProperty("--size", BOARD_SIZE)
-// let mineQuantityIndicator = document.querySelector("#mine-count")
-// mineQuantityIndicator.textContent = MINE_QUANTITY
-
-// let board = tileCreation(BOARD_SIZE, MINE_QUANTITY)
-
-// board.forEach((row) => {
-//   row.forEach((tile) => {
-//     boardContainer.append(tile.tileElement)
-
-//     tile.tileElement.addEventListener("click", () => {
-//       tileRevealing(tile, board, mineQuantityIndicator, boardContainer)
-//       gameResolutionChecker(tile, board, mineQuantityIndicator, boardContainer)
-//     })
-//     tile.tileElement.addEventListener("contextmenu", (e) => {
-//       e.preventDefault()
-
-//       tileMarking(tile, board, mineQuantityIndicator)
-//     })
-//   })
-// })
 
 // function gameResolutionChecker(tile, board, mineQuantityIndicator) {
 //   if (tile.status === "mine") {
