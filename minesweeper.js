6a93fb0c58c1b301f31ecd71bacbf74b4ac7fcc2
Danial A <danazad2016@gmail.com>
2021-10-06 18:39:38 -0500

    Added way to mark tiles and have it decrement amount of mines

--- minesweeper.js
+++ minesweeper.js
@@ -5,37 +5,29 @@ export const TILE_STATUSES = {
   MARKED: "marked",
 }
 
-export function boardPopulation(boardDimensions, mineQuantity) {
+const mineQuantityDisplayer = document.querySelector("#mine-count")
+const boardElement = document.querySelector(".board")
+
+export function boardTileCreation(DIMENSIONS, MINE_QUANTITY) {
   const board = []
 
-  const minedTiles = mineLocation(boardDimensions, mineQuantity)
-
-  for (let x = 0; x < boardDimensions; x++) {
+  for (let x = 0; x < DIMENSIONS; x++) {
     const row = []
 
-    for (let y = 0; y < boardDimensions; y++) {
+    for (let y = 0; y < DIMENSIONS; y++) {
       const tileElement = document.createElement("div")
-      tileElement.setAttribute("class", "tile")
       tileElement.dataset.status = TILE_STATUSES.HIDDEN
-
-      let mine = false
-
-      minedTiles.forEach((position) => {
-        if (position.x === x && position.y === y) {
-          mine = true
-        }
-      })
+      tileElement.classList.add("tile")
 
       const tile = {
+        tileElement,
         x,
         y,
-        mine,
-        tileElement,
         get status() {
           return this.tileElement.dataset.status
         },
-        set status(condition) {
-          this.tileElement.dataset.status = condition
+        set status(status) {
+          this.tileElement.dataset.status = status
         },
       }
 
@@ -45,313 +37,37 @@ export function boardPopulation(boardDimensions, mineQuantity) {
     board.push(row)
   }
 
-  board.forEach((row) => {
-    row.forEach((tile) => {
-      if (tile.mine) {
-        tile.tileElement.style.backgroundColor = "red"
-      }
-    })
-  })
-
   return board
 }
 
-export function tileRevealing(
-  tile,
-  board,
-  boardElement,
-  statusInformer,
-  MINE_QUANTITY,
-  replayContainer
-) {
-  if (tile.status === TILE_STATUSES.HIDDEN) {
-    if (tile.mine) {
-      tile.status = TILE_STATUSES.MINE
-
-      gameResolution(
-        tile,
-        board,
-        statusInformer,
-        boardElement,
-        MINE_QUANTITY,
-        replayContainer
-      )
-    } else {
-      tile.status = TILE_STATUSES.NUMBER
-
-      mineDistanceCalculation(tile, board)
-      gameResolution(
-        tile,
-        board,
-        statusInformer,
-        boardElement,
-        MINE_QUANTITY,
-        replayContainer
-      )
-    }
-  }
+export function leftClick() {
+  console.log("hi")
 }
 
-export function tileMarking(tile, board, mineQuantityInsight, MINE_QUANTITY) {
+export function rightClick(tile, MINE_QUANTITY) {
+  tile.status = TILE_STATUSES.MARKED
+
+  const numberOfMarkedTiles = markedTileNumeration()
+
+  mineQuantityDisplayer.textContent = Number(
+    MINE_QUANTITY - numberOfMarkedTiles.length
+  )
+}
+
+function markedTileNumeration() {
+  const tiles = Array.from(boardElement.children)
+
   const markedTiles = []
 
-  if (tile.status === TILE_STATUSES.HIDDEN) {
-    tile.status = TILE_STATUSES.MARKED
-  } else if (tile.status === TILE_STATUSES.MARKED) {
-    tile.status = TILE_STATUSES.HIDDEN
-  }
-
-  board.forEach((row) => {
-    row.forEach((tile) => {
-      if (tile.status === TILE_STATUSES.MARKED) {
-        markedTiles.push(tile)
-      }
-    })
-  })
-
-  mineQuantityInsight.textContent = Number(MINE_QUANTITY - markedTiles.length)
-
-  console.log(markedTiles)
-}
-
-function mineDistanceCalculation(tile, board) {
-  const neighbouringTiles = []
-  const surroundingMines = []
-
-  for (let x = -1; x <= 1; x++) {
-    for (let y = -1; y <= 1; y++) {
-      const neighbouringTile = board[tile.x + x]?.[tile.y + y]
-
-      if (neighbouringTile != null) {
-        neighbouringTiles.push(neighbouringTile)
-      }
-    }
-  }
-
-  neighbouringTiles.forEach((tile) => {
-    if (tile.mine) {
-      surroundingMines.push(tile)
+  tiles.forEach((tile) => {
+    if (tile.dataset.status === TILE_STATUSES.MARKED) {
+      markedTiles.push(tile)
     }
   })
 
-  tile.tileElement.textContent = surroundingMines.length
+  return markedTiles
 }
 
-function gameResolution(
-  tile,
-  board,
-  statusInformer,
-  boardElement,
-  MINE_QUANTITY,
-  replayContainer
-) {
-  if (tile.mine) {
-    boardElement.classList.add("defeat")
-
-    statusInformer.textContent = "You've hit a mine!"
-
-    replayContainer.classList.remove("off")
-  } else {
-    const revealedNumberTiles = []
-    const totalTiles = boardElement.children.length
-
-    console.log(totalTiles)
-
-    board.forEach((row) => {
-      row.forEach((tile) => {
-        if (tile.status === TILE_STATUSES.NUMBER) {
-          revealedNumberTiles.push(tile)
-        }
-      })
-    })
-
-    if (
-      Number(totalTiles - (revealedNumberTiles.length + MINE_QUANTITY)) === 0
-    ) {
-      statusInformer.textContent = "Victory!"
-
-      boardElement.classList.add("victory")
-      replayContainer.classList.remove("off")
-    }
-  }
+export function stateSetup(MINE_QUANTITY) {
+  mineQuantityDisplayer.textContent = MINE_QUANTITY
 }
-
-function mineLocation(boardDimensions, mineQuantity) {
-  const minePositions = []
-
-  while (minePositions.length < mineQuantity) {
-    const minePosition = {
-      x: randomMineLocation(boardDimensions),
-      y: randomMineLocation(boardDimensions),
-    }
-
-    let result = mineLocationUniquenessVerifer(minePosition, minePositions)
-
-    if (!result) {
-      minePositions.push(minePosition)
-    }
-  }
-
-  return minePositions
-}
-
-function randomMineLocation(boardDimensions) {
-  return Math.floor(Math.random() * boardDimensions)
-}
-
-function mineLocationUniquenessVerifer(minePosition, minePositions) {
-  let isUnique
-
-  minePositions.forEach((position) => {
-    const { x: xArray, y: yArray } = position
-    const { x, y } = minePosition
-
-    if (xArray === x && yArray === y) {
-      isUnique = false
-
-      return isUnique
-    }
-    return isUnique
-  })
-  return isUnique
-}
-
-// export const TILE_STATUSES = {
-//   HIDDEN: "hidden",
-//   MINE: "mine",
-//   NUMBER: "number",
-//   MARKED: "marked",
-// }
-
-// export function tileCreation(boardSize, numberOfMines) {
-//   const board = []
-
-//   const minePositions = createMineLocations(boardSize, numberOfMines)
-
-//   for (let x = 0; x < boardSize; x++) {
-//     const row = []
-//     for (let y = 0; y < boardSize; y++) {
-//       const tileElement = document.createElement("div")
-//       tileElement.dataset.status = TILE_STATUSES.HIDDEN
-
-//       const tile = {
-//         tileElement,
-//         x,
-//         y,
-//         mine: minePositions.some((arrayPosition) =>
-//           positionMatch(arrayPosition, { x, y })
-//         ),
-//         get status() {
-//           return this.tileElement.dataset.status
-//         },
-//         set status(value) {
-//           this.tileElement.dataset.status = value
-//         },
-//       }
-
-//       row.push(tile)
-//     }
-
-//     board.push(row)
-//   }
-
-//   return board
-// }
-
-// export function tileRevealing(
-//   tile,
-//   board,
-//   mineQuantityIndicator,
-//   boardContainer
-// ) {
-//   if (tile.status != "hidden") {
-//     return
-//   }
-
-//   if (tile.mine) {
-//     tile.status = TILE_STATUSES.MINE
-
-//     boardContainer.style.pointerEvents = "none"
-//   } else {
-//     tile.status = TILE_STATUSES.NUMBER
-
-//     const nearbyTiles = []
-//     const numberOfMinedTiles = []
-
-//     for (let xOffset = -1; xOffset <= 1; xOffset++) {
-//       for (let yOffset = -1; yOffset <= 1; yOffset++) {
-//         nearbyTiles.push(board[tile.x + xOffset]?.[tile.y + yOffset])
-//       }
-//     }
-
-//     nearbyTiles.forEach((tile) => {
-//       if (tile != undefined) {
-//         if (tile.mine) {
-//           numberOfMinedTiles.push(tile)
-//         }
-//       }
-//     })
-
-//     if (numberOfMinedTiles.length === 0) {
-//       nearbyTiles.forEach((tile) => {
-//         if (tile != null) {
-//           tileRevealing(tile, board, mineQuantityIndicator)
-//         }
-//       })
-//     } else {
-//       tile.tileElement.textContent = numberOfMinedTiles.length
-//     }
-//   }
-// }
-
-// export function tileMarking(tile, board, mineQuantityIndicator) {
-//   if (tile.status === "hidden") {
-//     tile.status = TILE_STATUSES.MARKED
-//   } else if (tile.status === "marked") {
-//     tile.status = TILE_STATUSES.HIDDEN
-//   }
-
-//   const markedTiles = []
-//   const minedTiles = []
-
-//   board.forEach((row) => {
-//     row.forEach((tile) => {
-//       if (tile.mine) {
-//         minedTiles.push(tile.tileElement)
-//       }
-
-//       if (tile.tileElement.dataset.status === "marked") {
-//         markedTiles.push(tile.tileElement)
-//       }
-//     })
-//   })
-
-//   mineQuantityIndicator.textContent = minedTiles.length - markedTiles.length
-// }
-
-// function createMineLocations(boardSize, numberOfMines) {
-//   const positions = []
-
-//   while (positions.length < numberOfMines) {
-//     const position = {
-//       x: randomPosition(boardSize),
-//       y: randomPosition(boardSize),
-//     }
-
-//     if (
-//       !positions.some((arrayPosition) => positionMatch(arrayPosition, position))
-//     ) {
-//       positions.push(position)
-//     }
-//   }
-
-//   return positions
-// }
-
-// function positionMatch(a, b) {
-//   return a.x === b.x && a.y === b.y
-// }
-
-// function randomPosition(boardSize) {
-//   return Math.floor(Math.random() * boardSize)
-// }
--- ui.js
+++ ui.js
@@ -1,279 +1,31 @@
-import { boardPopulation, tileRevealing, tileMarking } from "./minesweeper.js"
+import {
+  boardTileCreation,
+  leftClick,
+  rightClick,
+  stateSetup,
+} from "./minesweeper.js"
 
-let boardSizeRange = document.querySelector("#board-size-range")
-let mineQuantityRange = document.querySelector("#mine-quantity-range")
+const DIMENSIONS = 5
+const MINE_QUANTITY = 5
 
-let BOARD_DIMENSIONS = Number(boardSizeRange.value)
-let MINE_QUANTITY = 5
+const boardPopulating = boardTileCreation(DIMENSIONS, MINE_QUANTITY)
 
-let boardElement = document.querySelector(".board")
-boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
+const boardContainer = document.querySelector(".board")
+boardContainer.style.setProperty("--size", DIMENSIONS)
 
-let statusInformer = document.querySelector(".subtext")
-let mineQuantityInsight = document.querySelector("#mine-count")
-mineQuantityInsight.textContent = MINE_QUANTITY
-let replayContainer = document.querySelector(".game-redo-option")
-let boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-let boardSizeIndicator = document.querySelector(".board-size")
-let mineQuantity = document.querySelector(".mine-quantity")
+stateSetup(MINE_QUANTITY)
 
-let gameAdjustments = document.querySelectorAll(".game-adjustments")
-
-boardSizeIndicator.textContent = `${BOARD_DIMENSIONS}x${BOARD_DIMENSIONS}`
-mineQuantity.textContent = MINE_QUANTITY
-
-boardCreation.forEach((row) => {
+boardPopulating.forEach((row) => {
   row.forEach((tile) => {
-    boardElement.append(tile.tileElement)
+    boardContainer.append(tile.tileElement)
 
-    tile.tileElement.addEventListener("click", () => {
-      tileRevealing(
-        tile,
-        boardCreation,
-        boardElement,
-        statusInformer,
-        MINE_QUANTITY,
-        replayContainer
-      )
+    tile.tileElement.addEventListener("click", (e) => {
+      leftClick()
     })
-
     tile.tileElement.addEventListener("contextmenu", (e) => {
       e.preventDefault()
 
-      tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
+      rightClick(tile, MINE_QUANTITY)
     })
   })
 })
-
-replayContainer
-  .querySelector(".affirmative-button")
-  .addEventListener("click", gameRedo)
-
-function gameRedo() {
-  location.reload()
-}
-
-// boardSizeRange.addEventListener("input", (e) => {
-//   BOARD_DIMENSIONS = Number(boardSizeRange.value)
-//   boardSizeIndicator.textContent = `${BOARD_DIMENSIONS}x${BOARD_DIMENSIONS}`
-
-//   mineQuantityRange.min = BOARD_DIMENSIONS
-//   mineQuantityRange.max = BOARD_DIMENSIONS * 5
-
-//   const tiles = Array.from(boardElement.children)
-
-//   tiles.forEach((tile) => tile.remove())
-
-//   boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-//   boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
-
-//   boardCreation.forEach((row) => {
-//     row.forEach((tile) => {
-//       boardElement.append(tile.tileElement)
-
-//       tile.tileElement.addEventListener("click", () => {
-//         tileRevealing(
-//           tile,
-//           boardCreation,
-//           boardElement,
-//           statusInformer,
-//           MINE_QUANTITY,
-//           replayContainer
-//         )
-//       })
-
-//       tile.tileElement.addEventListener("contextmenu", (e) => {
-//         e.preventDefault()
-
-//         tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-//       })
-//     })
-//   })
-
-//   // let numberOfMines = []
-//   // let BOARD_DIMENSIONS = Number(boardSizeRange.value)
-//   // MINE_QUANTITY = mineQuantityRange.value
-//   // let boardElement = document.querySelector(".board")
-//   // boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
-//   // const tiles = Array.from(boardElement.children)
-//   // tiles.forEach((tile) => {
-//   //   if (tile.mine) {
-//   //     numberOfMines.push(tile)
-//   //   }
-//   // })
-//   // let currentTiles = Array.from(boardElement.children)
-//   // currentTiles.forEach((tile) => {
-//   //   tile.remove()
-//   // })
-//   // let boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-//   // boardSizeIndicator.textContent = `${BOARD_DIMENSIONS}x${BOARD_DIMENSIONS}`
-//   // boardCreation.forEach((row) => {
-//   //   row.forEach((tile) => {
-//   //     boardElement.append(tile.tileElement)
-//   //     tile.tileElement.addEventListener("click", () => {
-//   //       tileRevealing(
-//   //         tile,
-//   //         boardCreation,
-//   //         boardElement,
-//   //         statusInformer,
-//   //         MINE_QUANTITY,
-//   //         replayContainer
-//   //       )
-//   //     })
-//   //     tile.tileElement.addEventListener("contextmenu", (e) => {
-//   //       e.preventDefault()
-//   //       tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-//   //     })
-//   //   })
-//   // })
-// }) ***************
-
-gameAdjustments.forEach((range) => {
-  range.addEventListener("input", (e) => {
-    if (e.target.id === "mine-quantity-range") {
-      MINE_QUANTITY = e.target.value
-
-      const boardElementTiles = Array.from(boardElement.children)
-
-      boardElementTiles.forEach((tile) => tile.remove())
-
-      boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-      boardCreation.forEach((row) => {
-        row.forEach((tile) => {
-          boardElement.append(tile.tileElement)
-
-          tile.tileElement.addEventListener("click", () => {
-            tileRevealing(
-              tile,
-              boardCreation,
-              boardElement,
-              statusInformer,
-              MINE_QUANTITY,
-              replayContainer
-            )
-          })
-
-          tile.tileElement.addEventListener("contextmenu", (e) => {
-            e.preventDefault()
-
-            tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-          })
-        })
-      })
-    } else if (e.target.id === "board-size-range") {
-      BOARD_DIMENSIONS = e.target.value
-
-      const boardElementTiles = Array.from(boardElement.children)
-
-      boardElementTiles.forEach((tile) => tile.remove())
-
-      boardSizeIndicator.textContent = `${BOARD_DIMENSIONS}x${BOARD_DIMENSIONS}`
-      boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
-
-      mineQuantityRange.min = BOARD_DIMENSIONS
-      mineQuantityRange.max = BOARD_DIMENSIONS * 2
-
-      boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-      boardCreation.forEach((row) => {
-        row.forEach((tile) => {
-          boardElement.append(tile.tileElement)
-
-          tile.tileElement.addEventListener("click", () => {
-            tileRevealing(
-              tile,
-              boardCreation,
-              boardElement,
-              statusInformer,
-              MINE_QUANTITY,
-              replayContainer
-            )
-          })
-
-          tile.tileElement.addEventListener("contextmenu", (e) => {
-            e.preventDefault()
-
-            tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-          })
-        })
-      })
-    }
-  })
-})
-
-// mineQuantityRange.addEventListener("input", (e) => {
-//   MINE_QUANTITY = Number(mineQuantityRange.value)
-
-//   mineQuantity.textContent = MINE_QUANTITY
-
-//   const tiles = Array.from(boardElement.children)
-
-//   tiles.forEach((tile) => tile.remove())
-
-//   boardCreation = boardPopulation(BOARD_DIMENSIONS, MINE_QUANTITY)
-//   boardElement.style.setProperty("--size", BOARD_DIMENSIONS)
-
-//   boardCreation.forEach((row) => {
-//     row.forEach((tile) => {
-//       boardElement.append(tile.tileElement)
-
-//       tile.tileElement.addEventListener("click", () => {
-//         tileRevealing(
-//           tile,
-//           boardCreation,
-//           boardElement,
-//           statusInformer,
-//           MINE_QUANTITY,
-//           replayContainer
-//         )
-//       })
-
-//       tile.tileElement.addEventListener("contextmenu", (e) => {
-//         e.preventDefault()
-
-//         tileMarking(tile, boardCreation, mineQuantityInsight, MINE_QUANTITY)
-//       })
-//     })
-//   })
-// }) ***********
-
-// function gameResolutionChecker(tile, board, mineQuantityIndicator) {
-//   if (tile.status === "mine") {
-//     mineQuantityIndicator.textContent = "Hit a mine!"
-
-//     board.forEach((row) => {
-//       row.forEach((tile) => {
-//         if (tile.mine) {
-//           tile.status = "mine"
-//         }
-//         console.log(tile.tileElement)
-//         tile.tileElement.style.opacity = 0.5
-//       })
-//     })
-//   } else {
-//     let numberOfTiles = []
-//     let numberTiles = []
-//     let numberMinedTiles = []
-
-//     board.forEach((row) => {
-//       row.forEach((tile) => {
-//         numberOfTiles.push(tile)
-
-//         if (tile.mine) {
-//           numberMinedTiles.push(tile)
-//         }
-
-//         if (tile.status === TILE_STATUSES.NUMBER) {
-//           numberTiles.push(tile)
-//         }
-//       })
-//     })
-
-//     if (numberTiles.length + numberMinedTiles.length === numberOfTiles.length) {
-//       mineQuantityIndicator.textContent = "Alegría has ganado"
-
-//       numberMinedTiles.forEach((tile) => {
-//         tile.status = TILE_STATUSES.MARKED
-//       })
-//     }
-//   }
-// }
